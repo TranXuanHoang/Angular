9c1256ff31f67b5baedaefccb4672f49b9c75eb9
TranXuanHoang <hoangtx.ict@gmail.com>
2020-06-13 02:08:52 +0900

    Display a list of recipes

--- shopping-book/src/app/recipes/recipe-list/recipe-list.component.ts
+++ shopping-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe',
+      'This is a test recipe',
+      'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/chorizo-mozarella-gnocchi-bake-cropped.jpg'
+    ),
+    new Recipe('Another Test Recipe',
+      'This is another test recipe with a longer description. You can check the description here.',
+      'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/chorizo-mozarella-gnocchi-bake-cropped.jpg'
+    ),
+  ];
 
   constructor() { }
 
