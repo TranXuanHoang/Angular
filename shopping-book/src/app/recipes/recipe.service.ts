bdd544242acb62cd68a1e9d78cadee6da00c8453
TranXuanHoang <hoangtx.ict@gmail.com>
2020-06-16 01:08:23 +0900

    Passing ingredients to recipes using service injection

--- shopping-book/src/app/recipes/recipe.service.ts
+++ shopping-book/src/app/recipes/recipe.service.ts
@@ -1,7 +1,9 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipe.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
   private recipes: Recipe[] = [
     new Recipe(
@@ -23,7 +25,13 @@ export class RecipeService {
 
   recipeSelected = new EventEmitter<Recipe>();
 
+  constructor(private shoppingListService: ShoppingListService) {}
+
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredeints: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredeints);
   }
 }
