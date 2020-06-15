befecdab2f21392f69c846a1e8699360cd0881be
TranXuanHoang <hoangtx.ict@gmail.com>
2020-06-16 00:41:30 +0900

    Add ingredients to recipes

--- shopping-book/src/app/recipes/recipe.service.ts
+++ shopping-book/src/app/recipes/recipe.service.ts
@@ -1,16 +1,24 @@
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipe.model';
 
 export class RecipeService {
   private recipes: Recipe[] = [
-    new Recipe('Test Recipe',
-      'This is a test recipe',
-      'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/chorizo-mozarella-gnocchi-bake-cropped.jpg'
-    ),
-    new Recipe('Wheat Pizza',
-      'The Easiest Whole Wheat Pizza Dough.',
-      'https://cookieandkate.com/images/2018/08/whole-wheat-pizza-dough-1-1-768x1152.jpg'
-    ),
+    new Recipe(
+      'Tasty Schnitzel',
+      'A super-tasty Schnitzel - just awesome!',
+      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
+    new Recipe('Big Fat Burger',
+      'What else you need to say?',
+      'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ]),
   ];
 
   recipeSelected = new EventEmitter<Recipe>();
