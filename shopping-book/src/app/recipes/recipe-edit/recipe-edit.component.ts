c769168526e03c69f7c6434b4a495a8b990f8862
TranXuanHoang <hoangtx.ict@gmail.com>
2020-06-22 23:18:02 +0900

    Implement the recipe adding/editing form using reactive form
    * Add bootrap-icons dependency
    * Change from the two-way binding method used in template-driven forms to using a reactive form to construct the recipe adding/editing form

--- shopping-book/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ shopping-book/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -9,6 +11,7 @@ import { RecipeService } from '../recipe.service';
   styleUrls: ['./recipe-edit.component.css']
 })
 export class RecipeEditComponent implements OnInit {
+  recipeForm: FormGroup;
   id: number;
   editMode: boolean = false;
   recipe: Recipe;
@@ -20,15 +23,59 @@ export class RecipeEditComponent implements OnInit {
       (params: Params) => {
         this.id = +params['id'];
         this.editMode = params['id'] != null;
-        console.log(this.id);
-        console.log(this.editMode);
         if (this.editMode) {
           this.recipe = this.recipeService.getRecipe(this.id);
         } else {
           this.recipe = new Recipe('', '', '', []);
         }
+        this.initForm();
       }
     );
   }
 
+  private initForm() {
+    this.recipeForm = new FormGroup({
+      name: new FormControl(this.recipe.name, Validators.required),
+      description: new FormControl(this.recipe.description, Validators.required),
+      imagePath: new FormControl(this.recipe.imagePath, Validators.required),
+      ingredients: new FormArray(
+        this.recipe.ingredients.map(
+          (ingredient: Ingredient, index: number) => {
+            return new FormGroup({
+              'name': new FormControl(ingredient.name, Validators.required),
+              'amount': new FormControl(ingredient.amount, [
+                Validators.required,
+                Validators.pattern(/^[1-9]+[0-9]*$/)
+              ])
+            });
+          })
+      )
+    });
+  }
+
+  get ingredientsControls() {
+    return (this.recipeForm.get('ingredients') as FormArray).controls;
+  }
+
+  onAddIngredient() {
+    (<FormArray>this.recipeForm.get('ingredients')).push(new FormGroup({
+      'name': new FormControl(null, Validators.required),
+      'amount': new FormControl(null, [
+        Validators.required,
+        Validators.pattern(/^[1-9]+[0-9]*$/)
+      ])
+    }));
+  }
+
+  onDeleteIngredient(index: number) {
+    (this.recipeForm.get('ingredients') as FormArray).removeAt(index);
+  }
+
+  onSubmit() {
+    console.log(this.recipeForm);
+  }
+
+  onCancel() {
+    this.recipeForm.reset();
+  }
 }
