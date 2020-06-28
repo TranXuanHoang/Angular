47281ba4ee46d96522c54eca408c64b049ed8399
TranXuanHoang <hoangtx.ict@gmail.com>
2020-06-28 10:22:47 +0900

    Protect routes using a route guard
    * Protect /recipes and all its child routes from accessing without being authenticated by using a route guard

--- shopping-book/src/app/app-routing.module.ts
+++ shopping-book/src/app/app-routing.module.ts
@@ -7,16 +7,23 @@ import { RecipeResolver } from './recipes/recipe-detail/recipe-resolver.service'
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipesResolverService } from './recipes/recipes-resolver.service';
 import { AuthComponent } from './auth/auth.component';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   {
+    path: 'recipes',
+    component: RecipesComponent,
+    // The canActivate: [...] here will check whether the user is authenticated (logged in).
+    // If yes, then it will allow navigating to /recipes. Otherwise, it will direct to /auth.
+    canActivate: [AuthGuard],
     // The resolver: [...] here will let the /recipes route wait until the 'RecipesResolverService'
     // finish fetching data from Firebase Realtime Database and set the data into the 'RecipeService'.
     // This is important, so that the app get recipes data populated before users navigate to sub-routes.
     // This setup here also means that whenever users navigate to routers other than
     // /recipes[|/new|/:id|/:id/edit], and then navigate back to /recipes[|/new|/:id|/:id/edit]
     // the RecipesResolverService will get its 'resolve' method run again
-    path: 'recipes', component: RecipesComponent, resolve: [RecipesResolverService], children: [
+    resolve: [RecipesResolverService],
+    children: [
       { path: 'new', component: RecipeEditComponent },
       { path: ':id', component: RecipeDetailComponent, resolve: { recipe: RecipeResolver } },
       { path: ':id/edit', component: RecipeEditComponent },
