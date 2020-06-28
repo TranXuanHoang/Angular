5dc2fc616a4a5bb5e91021640ab90ee73639876a
TranXuanHoang <hoangtx.ict@gmail.com>
2020-06-29 01:16:45 +0900

    Group authentication code into a separate module

--- shopping-book/src/app/app.module.ts
+++ shopping-book/src/app/app.module.ts
@@ -1,33 +1,28 @@
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AuthInterceptorService } from './auth/auth-interceptor.service';
-import { AuthComponent } from './auth/auth.component';
-import { HeaderComponent } from './header/header.component';
-import { RecipeService } from './recipes/recipe.service';
-import { RecipesModule } from './recipes/recipes.module';
-import { ShoppingListModule } from './shopping-list/shopping-list.module';
-import { ShoppingListService } from './shopping-list/shopping-list.service';
-import { SharedModule } from './shared/shared.module';
+import { AuthModule } from './auth/auth.module';
 import { CoreModule } from './core.module';
+import { HeaderComponent } from './header/header.component';
+import { RecipesModule } from './recipes/recipes.module';
+import { SharedModule } from './shared/shared.module';
+import { ShoppingListModule } from './shopping-list/shopping-list.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    AuthComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
     HttpClientModule,
+    AppRoutingModule,
+    AuthModule,
     RecipesModule,
     ShoppingListModule,
     SharedModule,
