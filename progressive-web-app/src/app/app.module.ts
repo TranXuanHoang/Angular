7d1cc671df5b8a9816662a09550ae7b4293f7d34
TranXuanHoang <hoangtx.ict@gmail.com>
2020-07-06 18:00:15 +0900

    Add a service worker to the progressive-web-app

--- progressive-web-app/src/app/app.module.ts
+++ progressive-web-app/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostComponent } from './post/post.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,8 @@ import { PostComponent } from './post/post.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
