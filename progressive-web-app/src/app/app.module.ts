6e1e3c70ef8078f8e72a6cb5ebf52d4213b53273
TranXuanHoang <hoangtx.ict@gmail.com>
2020-07-06 15:51:11 +0900

    Implement main logic of fetching and showing posts

--- progressive-web-app/src/app/app.module.ts
+++ progressive-web-app/src/app/app.module.ts
@@ -1,15 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
